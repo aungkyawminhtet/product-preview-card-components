012344ad3eff905981330de3c7dac3c11705172c
aungkyawminhtet <aungkyawminhtet.sbo@gmail.com>
2026-01-04 23:19:43 +0700

    fix build err

--- src/components/AddToCardBtn.tsx
+++ src/components/AddToCardBtn.tsx
@@ -1,4 +1,4 @@
-import React, { type ReactNode, useState } from "react";
+import { type ReactNode, useState } from "react";
 import cardIcon from "../assets/icon-cart.svg";
 import "./AddToCardBtn.css";
 
